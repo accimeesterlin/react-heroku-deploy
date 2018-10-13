01a9359e2231a77182ab993759907ef01de14630
Esterling Accime <esterling@brainbytescreative.com>
2018-10-12 23:25:16 -0400

    remove build folder in gitignore

---
+++ client/build/precache-manifest.835e7e549e737ff0d33491b644cf52c0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "957f912e4e1fbebae0aa",
+    "url": "/static/js/main.957f912e.chunk.js"
+  },
+  {
+    "revision": "ff362e4f2cd8684ee76b",
+    "url": "/static/js/1.ff362e4f.chunk.js"
+  },
+  {
+    "revision": "957f912e4e1fbebae0aa",
+    "url": "/static/css/main.939722e8.chunk.css"
+  },
+  {
+    "revision": "f4c3fe3ae2c620239625fb2b03490ca5",
+    "url": "/index.html"
+  }
+];
